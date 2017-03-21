e87a36cd30a4ad62ded007b5377b16293cc564f8
softPigeon <pogodin@phystech.edu>
2017-03-21 19:49:10 +0300

    Parallel Computing Toolbox support added

--- get_schatten_statistics.m
+++ get_schatten_statistics.m
@@ -1,15 +1,11 @@
 function get_schatten_statistics(prob, graph_size, num_trials, ...
-    filename, buff_size, methods, is_rankone_start)
+    filename, methods, is_rankone_start)
 
-if nargin < 7
+if nargin < 6
    is_rankone_start = false; 
 end
-if nargin < 6
-    methods = {'schatten'};
-end
-
 if nargin < 5
-    buff_size = 100;
+    methods = {'schatten'};
 end
 
 num_cut_finder_trials = 10;
@@ -21,33 +17,60 @@ num_iter = 10;
 precision = 0.001;
 rank_tolerance = 1e-3;
 
-data = zeros(min(num_trials, buff_size), 3 * length(methods));
+if ~exist(filename, 'file')   
+    fid = fopen(filename, 'w');
+    fclose(fid);
+    disp(strcat('File "', filename, '" created'));
+end
 
-for i = 0:num_trials - 1
-    try
-        laplacian_matrix = get_laplacian('random', prob, graph_size);
+v = ver;
+if any(strcmp('Parallel Computing Toolbox', {v.Name}))
+    parfor i = 0:num_trials - 1
+        try
+            laplacian_matrix = get_laplacian('random', prob, graph_size);
 
-        return_values_map = solve_maxcut_all(laplacian_matrix, methods, ...
-            p, eps, num_iter, precision, num_cut_finder_trials, ...
-            is_quiet, is_cvx_quiet, rank_tolerance, is_rankone_start);
+            return_values_map = solve_maxcut_all(laplacian_matrix, methods, ...
+                p, eps, num_iter, precision, num_cut_finder_trials, ...
+                is_quiet, is_cvx_quiet, rank_tolerance, is_rankone_start);
 
-        data(1 + mod(i, buff_size), ...
-            1:length(return_values_map.values)) = ...
-            cell2mat(return_values_map.values);
-        
-        if ~exist(filename, 'file')   
-            fid = fopen(filename, 'w');
-            fprintf(fid, strcat(strjoin(return_values_map.keys, ','), '\n'));
-            fclose(fid);
-            disp('File created');
+            dlmwrite(filename, cell2mat(return_values_map.values), ...
+                'delimiter', ',', '-append');
+
+        catch ME
+            disp(ME.message);
         end
-    catch ME
-        disp(ME.message);
     end
-    
-    if ~mod(i + 1, buff_size) || i == num_trials -1
-        dlmwrite(filename, data, 'delimiter', ',', '-append');
-        disp('Data slice recorded');
+else
+    buff_size = 100;
+    data = zeros(min(num_trials, buff_size), 3 * length(methods));
+
+    for i = 0:num_trials - 1
+        try
+            laplacian_matrix = get_laplacian('random', prob, graph_size);
+
+            return_values_map = solve_maxcut_all(laplacian_matrix, methods, ...
+                p, eps, num_iter, precision, num_cut_finder_trials, ...
+                is_quiet, is_cvx_quiet, rank_tolerance, is_rankone_start);
+
+            data(1 + mod(i, buff_size), ...
+                1:length(return_values_map.values)) = ...
+                cell2mat(return_values_map.values);
+
+            if ~exist(filename, 'file')   
+                fid = fopen(filename, 'w');
+                fprintf(fid, strcat(strjoin(return_values_map.keys, ','), '\n'));
+                fclose(fid);
+                disp('File created');
+            end
+        catch ME
+            disp(ME.message);
+        end
+
+        if ~mod(i + 1, buff_size) || i == num_trials -1
+            dlmwrite(filename, data, 'delimiter', ',', '-append');
+            disp('Data slice recorded');
+        end
     end
 end
+    
 end

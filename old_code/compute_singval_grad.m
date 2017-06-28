394444514846138b40d43f737d34f55d1361bef0
softPigeon <pogodin@phystech.edu>
2017-06-28 16:44:29 +0300

    Bachelor thesis results added, some old files deleted

--- old_code/compute_singval_grad.m
+++
@@ -1,4 +0,0 @@
-function grad = compute_singval_grad(matrix, q, eps)
-    grad = 2 * eps * (1 + eps ^ q) * mpower(matrix * transpose(matrix) + ...
-        eps * eye(size(matrix, 1)), -2) * matrix;
-end

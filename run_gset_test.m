a89a44cee9aed02ed258c0533b1b568dc26ccc95
softPigeon <pogodin@phystech.edu>
2017-07-08 03:25:58 +0300

    Gset is solved for different methods at one time

--- run_gset_test.m
+++ run_gset_test.m
@@ -3,8 +3,7 @@ function run_gset_test(graph_numbers, method, is_constraint_relaxed, solver)
 %   from https://web.stanford.edu/%7Eyyye/yyye/Gset/
 %   RUN_GSET_TEST(graph_numbers) -- downloads Gset graphs, which numbers
 %   specified by a vector graph_number
-%   method -- method to improve an SDP solution: singval, irls, grad,
-%   logdet
+%   method -- method to improve an SDP solution: singval, grad, all (which is singval + grad)
 %   is_constraint_relaxed -- wheter to use 4W <= Tr(LX) or 4SDP=Tr(LX)
 %   solver -- which solver to use for SDP solution: cvx or sdplr
 
@@ -28,7 +27,7 @@ end
 %% Preliminaries
 eps = 0.005;
 q = 0.8;
-p = 0.01;
+p_param = [0.01, 0.1];
 num_iter = 100;
 precision = 1e-5;
 write_precision = 5;
@@ -37,7 +36,7 @@ num_cut_finder_trials = 10000;
 check_psd = false;  
 
 rank_tol_one = 1e-4;
-rank_tol_two = 1e-6; 
+rank_tol_two = 1e-5; 
 
 % colnames = {'rank', 'max_cut', 'cut_mean', 'cut_std'};
 results_sdp = zeros(1, 5);  
@@ -45,14 +44,6 @@ results_relaxed = zeros(1, 5);
 
 folder = 'gset_results/';
 mkdir(folder);
-
-if strcmp(method, 'irls') || strcmp(method, 'grad')
-    method_name = strcat(method, '_p', int2str(100 * p));
-elseif strcmp(method, 'singval')
-    method_name = strcat(method, '_q', int2str(100 * q));
-else
-    method_name = method;    
-end
 
 %% Solutions
 for n_graph = graph_numbers
@@ -75,54 +66,61 @@ for n_graph = graph_numbers
         compute_cut_randomized(graph_laplacian, ...
         sdp_matrix, num_cut_finder_trials);
     
-    %% Relaxation
-    graph_laplacian = full(graph_laplacian);
+    disp('Writing results...');
+    dlmwrite(strcat(folder, 'sdp_', general_name), ...
+        results_sdp, 'precision', write_precision); 
     
-    if strcmp(method, 'singval')
+    %% Relaxation    
+    if strcmp(method, 'singval') || strcmp(method, 'all')
+        %% Solving
         disp('...singular values');
         [matrix, ~] = solve_maxcut_singval(...
             graph_laplacian, sdp_optval, cut_optval, ...
             sdp_matrix, q, eps, num_iter, precision, false, true, ...
             is_constraint_relaxed, check_psd);
-    elseif strcmp(method, 'logdet')
-        disp('...logdet');
-        [matrix, ~] = solve_maxcut_logdet(...
-            graph_laplacian, sdp_optval, cut_optval, ...
-            sdp_matrix, eps, num_iter, precision, false, true, ...
-            is_constraint_relaxed);
-    elseif strcmp(method, 'grad')
-        disp('...Schatten grad');
-        [matrix, ~] = solve_maxcut_grad(...
-            graph_laplacian, sdp_optval, cut_optval, ...
-            sdp_matrix, p, eps, num_iter, precision, false, true, ...
-            is_constraint_relaxed);
-    elseif strcmp(method, 'irls')
-        disp('...Schatten IRLS');
-        [matrix, ~] = solve_maxcut_irls(...
-            graph_laplacian, sdp_optval, cut_optval, ...
-            sdp_matrix, p, eps, num_iter, precision, false, true, ...
-            is_constraint_relaxed); 
-    end
         
+        %% Writing results
+        disp('Writing results...');
+        method_name = strcat(method, '_q', int2str(100 * q));
+        
+        results_relaxed(1, 1) = rank(matrix, rank_tol_one);
+        results_relaxed(1, 2) = rank(matrix, rank_tol_two);
+
+        [~, results_relaxed(1, 3), results_relaxed(1, 4), results_relaxed(1, 5)] = ...
+            compute_cut_randomized(graph_laplacian, ...
+            matrix, num_cut_finder_trials, precision);
+        
+        
+        dlmwrite(strcat(folder, method_name, '_', general_name), ...
+            results_relaxed, 'precision', write_precision); 
+        disp('...done');
+    end
     
-    results_relaxed(1, 1) = rank(matrix, rank_tol_one);
-    results_relaxed(1, 2) = rank(matrix, rank_tol_two);
-    [~, results_relaxed(1, 3), results_relaxed(1, 4), results_relaxed(1, 5)] = ...
-        compute_cut_randomized(graph_laplacian, ...
-        matrix, num_cut_finder_trials, precision);
+    if strcmp(method, 'grad') || strcmp(method, 'all')
+        for p = p_param
+            %% Solving
+            disp(strcat('...Schatten grad for p', int2str(100 * p)));
+            [matrix, ~] = solve_maxcut_grad(...
+                graph_laplacian, sdp_optval, cut_optval, ...
+                sdp_matrix, p, eps, num_iter, precision, false, true, ...
+                is_constraint_relaxed);
+            
+            %% Writing results
+            disp('Writing results...');
+            method_name = strcat(method, '_p', int2str(100 * p));
+
+            results_relaxed(1, 1) = rank(matrix, rank_tol_one);
+            results_relaxed(1, 2) = rank(matrix, rank_tol_two);
+            
+            [~, results_relaxed(1, 3), results_relaxed(1, 4), results_relaxed(1, 5)] = ...
+                compute_cut_randomized(graph_laplacian, ...
+                matrix, num_cut_finder_trials, precision);
+            
+            dlmwrite(strcat(folder, method_name, '_', general_name), ...
+                results_relaxed, 'precision', write_precision); 
+            disp('...done');
+        end
+    end
     
-    disp('...done');
-    
-    %% Writing results
-    disp('Writing results...');
-    
-    dlmwrite(strcat(folder, 'sdp_', general_name), ...
-        results_sdp, 'precision', write_precision); 
-    
-    dlmwrite(strcat(folder, method_name, '_', general_name), ...
-        results_relaxed, ...
-        'precision', write_precision); 
-    
-    disp('...done');
 end
 end

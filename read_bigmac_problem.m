59f81c27b4faed0d08adde96491bb2571a37b1c9
softPigeon <pogodin@phystech.edu>
2017-07-24 02:28:56 +0300

    Minor fixes

--- read_bigmac_problem.m
+++ read_bigmac_problem.m
@@ -1,7 +1,7 @@
 function objective_matrix = read_bigmac_problem(problem_size, problem_number)
 %READ_BIGMAC_PROBLEM Reads a Bigmac 0-1 problem
 %   objective_matrix = READ_BIGMAC_PROBLEM(problem_size, problem_number) 
-%   reads a 0-1 min problem, invertes to make a max problem and    
+%   reads a 0-1 min problem, invertes it to make a max problem    
 %   http://biqmac.uni-klu.ac.at/library/biq/beasley/bqp#1-#2.sparse, where #1 =
 %   problems_size, #2 = problem_number
 
@@ -12,14 +12,13 @@ data = webread(url, options);
 data = strsplit(data, {' ', '\n'});
 data = cellfun(@str2num, data(1:length(data) - 1));
 
-graph_size = data(1);
 data = data(3:end);
 data = transpose(reshape(data, 3, length(data) / 3));
 
 objective_matrix = sparse(data(1:end, 1), ...
     data(1:end, 2), ...
     data(1:end, 3), ...
-    graph_size, graph_size);
+    problem_size, problem_size);
 
 objective_matrix = objective_matrix + objective_matrix.' - ...
     diag(diag(objective_matrix));
